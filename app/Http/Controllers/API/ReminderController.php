eed937612109b620b72e8bce8500893a82e61e48
Freddy <freddy@kresna.xyz>
2020-07-13 02:49:09 +0000

    change permission on storage and move ReminderController inside API

--- app/Http/Controllers/API/ReminderController.php
+++ app/Http/Controllers/API/ReminderController.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Log;
 use Illuminate\Http\Request;
 use Nexmo\Laravel\Facade\Nexmo;
+use App\Http\Controllers\Controller;
 
 class ReminderController extends Controller
 {
-    public function serviceScheduler(Request $request) {
-        if ($this->validateKey($request->get('key'), $request->get('to'))){
+    public function serviceSchedule(Request $request) {
+	if ($this->validateKey($request->get('key'), $request->get('to'))){
             Nexmo::message()->send([
                 'to'=> $request->get('to'),
                 'from' => config('nexmo.phone_number'),
@@ -24,7 +26,7 @@ class ReminderController extends Controller
     private function validateKey($key, $to) {
         $decodeKey = base64_decode($key);
         $results = explode(';', $decodeKey);
-        if ($results[0] == env('REMINDER_SALT') && $results[1] == $to) {
+	if ($results[0] == env('REMINDER_SALT') && $results[1] == $to) {
             return true;
         }
 
