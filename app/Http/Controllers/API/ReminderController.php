a0d10057a1491385ece65d0f0ad17d2400d13adc
fkresna <freddy@kresna.xyz>
2020-07-26 05:27:43 -0400

    add US Code

--- app/Http/Controllers/API/ReminderController.php
+++ app/Http/Controllers/API/ReminderController.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\Controller;
 
 class ReminderController extends Controller
 {
-    public function serviceSchedule(Request $request) {
-	if ($this->validateKey($request->get('key'), $request->get('to'))){
+    public function serviceSchedule(Request $request)
+    {
+        if ($this->validateKey($request->get('key'), $request->get('to'))) {
             Nexmo::message()->send([
-                'to'=> $request->get('to'),
+                'to' => $this->addUSCode($request->get('to')),
                 'from' => config('nexmo.phone_number'),
                 'text' => $request->get('body')
             ]);
@@ -23,13 +24,22 @@ class ReminderController extends Controller
         return false;
     }
 
-    private function validateKey($key, $to) {
+    private function validateKey($key, $to)
+    {
         $decodeKey = base64_decode($key);
         $results = explode(';', $decodeKey);
-	if ($results[0] == env('REMINDER_SALT') && $results[1] == $to) {
+        if ($results[0] == env('REMINDER_SALT') && $results[1] == $to) {
             return true;
         }
 
         return false;
+    }
+
+    private function addUSCode($number)
+    {
+        if (substr($number, 0, 2) != "+1")
+            return "+1" . $number;
+
+        return $number;
     }
 }
